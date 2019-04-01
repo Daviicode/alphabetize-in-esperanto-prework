5c010e53096044f8e88df41e84c606849bf79c39
David zhang <d.ydzdavid@yahoo.com>
2019-04-01 03:52:25 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,10 +1,8 @@
 def alphabetize(arr)
   # code here
-  arr.each_with_index do |element, index|
-    if index != arr.length - 1
-      should_be_swaped?(element, arr[index+1]) ? (return false) : (next)
-    else
-      return true
+  arr.sort_by do |word|
+    word.split('').collect do |letter|
+      esperanto_alphabet.index(letter)
     end
   end
 end
