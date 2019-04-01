476ce0e2e4f4e73b680c54d35400d9de82a18378
David zhang <d.ydzdavid@yahoo.com>
2019-04-01 03:51:40 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,10 @@
 def alphabetize(arr)
   # code here
-  arr.sort_by do |word|
-    word.chars.map do |phrase| 
-      alphabet.index(phrase)
+  arr.each_with_index do |element, index|
+    if index != arr.length - 1
+      should_be_swaped?(element, arr[index+1]) ? (return false) : (next)
+    else
+      return true
     end
   end
 end
